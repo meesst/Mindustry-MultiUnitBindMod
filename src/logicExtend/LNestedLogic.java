112c1557620f1f816e81627ceadbd11efb5acf11
zhang <zhang@example.com>
2026-03-07 16:34:30 +0800

    修复编译错误，将currentUniqueId变量定义移到try块外面

--- src/logicExtend/LNestedLogic.java
+++ src/logicExtend/LNestedLogic.java
@@ -519,11 +519,12 @@ public class LNestedLogic {
                             return;
                         }
                         
+                        String currentUniqueId = this.uniqueId;
                         try {
                             nestedDepth.set(nestedDepth.get() + 1);
                             
                             // 动态获取最新的uniqueId值
-                            String currentUniqueId = this.uniqueId;
+                            currentUniqueId = this.uniqueId;
                             log("开始执行call指令，逻辑名称: " + p1 + "，唯一编号: " + currentUniqueId);
                             
                             LExecutor nestedExec;
